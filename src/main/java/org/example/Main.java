212ba1d311516ff7fa7ecb12014e1a454bc471c6
Rushel <rushelsilvester@outlook.com>
2023-09-14 09:56:25 +0200

    changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -24,46 +24,134 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
-class OpenAiApiExample
+class Main
 {
+    private static JSONObject supplierObject = new JSONObject();
     private String TOKEN = "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";
 
     public static void main(String... args) throws InterruptedException
     {
 
         String token = "ZgiXRZupInt7HlViV75XsJXqzVnMLv_7NXSjS1sDvn6cfHfHFWXkDAGVOPYLenMWdqK6pA.;sidts-CjEBSAxbGb3wO-rlV6T6xS5pxzy7VXJe0F7GgV5_IfUMQ72PMJ5fQY3c6NwcYbgnQ8xdEAA";
-        AIClient client = new GoogleBardClient(token);
+//        AIClient client = new GoogleBardClient(token);
         //        openAiChat(TOKEN);
         JSONObject jsonObject = getJson();
 //        getFullJson(jsonObject);
-        String sup = getItSupplier(jsonObject);
-//        Answer answer = null;
+//        String sup = getItSupplier(jsonObject);
+        Answer answer = null;
 //        answer = client.ask("remember what i send you now !");
-        String chatGPT_Prompt = workingHours(jsonObject);
-    }
+//        String chatGPT_Prompt = workingHours(jsonObject);
 
-    private static String workingHours(JSONObject jsonObject)
-    {
-        String output = "{ \"WorkingHours\": [";
-        JSONArray workingHours = (JSONArray) jsonObject.get("IT_SUPPLIED_TIMEFRAMEGROUP");
-
-        for(int i = 0; i < workingHours.size(); i++)
+        JSONObject promtObject = createObject(jsonObject);
+        String[] chatGPT_Prompt = createPromt(promtObject);
+        for(int i = 0; i < chatGPT_Prompt.length; i++)
         {
-            JSONObject jsonObject1 = (JSONObject) workingHours.get(i);
-            String prompt = (String) jsonObject1.get("ID_REF");
-            String start = (String) jsonObject1.get("START_TIME");
-            String end = (String) jsoget("END_TIME");
-            String du
-            output += "{\"Person\": \"" + prompt + "\", \"Start\":\"" + start + "\", \"End\":\"" + end + "\"}";
-            if(i < workingHours.size() - 1)
-            {
-                output += ",";
-            }
-
+            System.out.println(chatGPT_Prompt[i]);
         }
-        return null;
     }
 
+    private static String[] createPromt(JSONObject promtObject) {
+        String[] prompts;
+        JSONArray jsonArray = (JSONArray) promtObject.get("Suppliers");
+        prompts = new String[jsonArray.size()];
+        for (int i = 0; i < jsonArray.size(); i++)
+        {
+            JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+            JSONArray skills = (JSONArray) jsonObject.get("Skills");
+            JSONArray timeFrame = (JSONArray) jsonObject.get("TimeFrame");
+            StringBuilder prompt = new StringBuilder();
+            prompt.append("Supplier: " + jsonObject.get("SupplierID") + "\n");
+            prompt.append("Skills:");
+            for (int j = 0; j < skills.size(); j++)
+            {
+                JSONObject skill = (JSONObject) skills.get(j);
+                prompt.append("\n  Skill Name "+skill.get("Skill_NAME") + "\n");
+                prompt.append("  Skill Level "+skill.get("Skill_LEVEL") + "");
+            }
+            prompt.append("\n");
+            prompt.append("TimeFrame:");
+            for (int j = 0; j < timeFrame.size(); j++)
+            {
+                JSONObject time = (JSONObject) timeFrame.get(j);
+                prompt.append("\n  Start Date: "+time.get("TimeFrameStartDate") + "\n");
+                prompt.append("  End Date: "+time.get("TimeFrameEndDate") + "\n");
+                prompt.append("  Start Time: "+time.get("TimeFrameStartTime") + "\n");
+                prompt.append("  End Time: "+time.get("TimeFrameEndTime") + "");
+            }
+            prompts[i] = prompt.toString();
+        }
+
+        return prompts;
+    }
+
+    private static JSONObject createObject(JSONObject jsonObject) {
+        //Add Supplier
+        getSupplierObject().put("Suppliers", new JSONArray());
+        JSONArray array = (JSONArray) getSupplierObject().get("Suppliers");
+        JSONArray jsonArray = (JSONArray) jsonObject.get("IT_SUPPLIER");
+        for (int i = 0; i < jsonArray.size(); i++)
+        {
+            JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+            JSONObject supplier = new JSONObject();
+
+            supplier.put("SupplierID", jsonObject1.get("ID_REF"));
+            supplier.put("SupplierLat", jsonObject1.get("LATITUDE"));
+            supplier.put("SupplierLon", jsonObject1.get("LONGITUDE"));
+            supplier.put("Skills", new JSONArray());
+            supplier.put("TimeFrame", new JSONArray());
+            array.add(supplier);
+        }
+        getSupplierObject().put("Suppliers", array);
+
+        //ADD TimeFrame
+        jsonArray = (JSONArray) jsonObject.get("IT_SUPPLIED_TIMEFRAMEGROUP");
+        for (int i = 0; i < jsonArray.size(); i++)
+        {
+            for(int j = 0; j < array.size(); j++)
+            {
+                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+                JSONObject supplier = (JSONObject) array.get(j);
+                if(jsonObject1.get("ID_REF").equals(supplier.get("SupplierID")))
+                {
+                    JSONArray timeFrame = (JSONArray) supplier.get("TimeFrame");
+                    JSONObject timeFrameObject = new JSONObject();
+                    timeFrameObject.put("TimeFrameStartDate", jsonObject1.get("START_DATE"));
+                    timeFrameObject.put("TimeFrameEndDate", jsonObject1.get("END_DATE"));
+                    timeFrameObject.put("TimeFrameStartTime", jsonObject1.get("START_TIME"));
+                    timeFrameObject.put("TimeFrameEndTime", jsonObject1.get("END_TIME"));
+                    timeFrame.add(timeFrameObject);
+                    supplier.put("TimeFrame", timeFrame);
+                    array.set(j, supplier);
+                }
+            }
+        }
+
+        //ADD SkillGroup
+        jsonArray = (JSONArray) jsonObject.get("IT_SUPPLIED_SKILLGROUP");
+        for (int i = 0; i < jsonArray.size(); i++)
+        {
+            for(int j = 0; j < array.size(); j++)
+            {
+                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+                JSONObject supplier = (JSONObject) array.get(j);
+                if(jsonObject1.get("ID_REF").equals(supplier.get("SupplierID")))
+                {
+                    JSONArray skills = (JSONArray) supplier.get("Skills");
+                    JSONObject skillObject = new JSONObject();
+                    skillObject.put("Skill_NAME", jsonObject1.get("SKILL_NAME"));
+                    skillObject.put("Skill_LEVEL", jsonObject1.get("SKILL_LEVEL"));
+                    skills.add(skillObject);
+                    supplier.put("Skills", skills);
+                    array.set(j, supplier);
+                }
+            }
+        }
+        return getSupplierObject();
+    }
+
+    public static JSONObject getSupplierObject() {
+        return supplierObject;
+    }
     private static String getItSupplier(JSONObject jsonObject)
     {
         StringBuilder suppliers = new StringBuilder("{ \\\"Supplier\\\": [{\" ");
@@ -118,24 +206,7 @@ class OpenAiApiExample
     return suppliers.toString();
     }
 
-    private static void getFullJson(JSONObject jsonObject)
-    {
-        for (Iterator iterator = jsonObject.keySet().iterator(); iterator.hasNext(); )
-        {
-            String key = (String) iterator.next();
-            System.out.println("--" + key);
-            for(int i = 0; jsonObject.get(key) instanceof JSONArray && i < ((JSONArray) jsonObject.get(key)).size(); i++)
-            {
-                JSONObject jsonObject1 = (JSONObject) ((JSONArray) jsonObject.get(key)).get(i);
-                System.out.println("--");
-                for (Iterator iterator1 = jsonObject1.keySet().iterator(); iterator1.hasNext(); )
-                {
-                    String key1 = (String) iterator1.next();
-                    System.out.println("----" + key1 + ": " + jsonObject1.get(key1));
-                }
-            }
-        }
-    }
+
 
     private void openAiChat(String token)
     {
@@ -151,7 +222,7 @@ class OpenAiApiExample
         JSONParser parser = new JSONParser();
         try
         {
-            Object obj = parser.parse(new FileReader("/System/Volumes/Data/mps/prj/ChatGPT-PromptTest/src/main/resources/request-test.json"));
+            Object obj = parser.parse(new FileReader("src/main/resources/request-test.json"));
 
             return (JSONObject) obj;
         } catch (IOException | ParseException e)
